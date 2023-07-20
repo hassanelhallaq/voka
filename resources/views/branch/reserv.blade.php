78f1902137ecbe37fededcbba670faaa4c4339dd
server <vkoanet@az1-ss106.a2hosting.com>
2023-07-20 14:25:18 -0700

    calnder-style

--- resources/views/branch/reserv.blade.php
+++ resources/views/branch/reserv.blade.php
@@ -1,5 +1,30 @@
    <style>
-
+    .fc-day-number{
+        color: #fff;
+   }
+    .fc-widget-header thead tr{
+            color: #fff;
+            font-size: 20px;
+    }
+    tbody td{
+             border-radius: 0;
+    }
+    .fc-unthemed td.fc-today .fc-day-number{
+        color:#fff;
+    }
+    .fc-state-active, .fc-state-disabled, .fc-state-down, .fc-state-top, .fc-state-hover{
+    color: #fff !important;
+    background-color:#e5772a !important;
+    }
+    .fc-state-hover{
+        background-position: 0 2.1em !important;
+    }
+    tbody td:hover{
+        border:1px solid #ddd;
+    }
+    .fc-unthemed td.fc-today{
+        background-color:#e5772a !important;
+    }
    </style>
    <div id="mainPage">
        <div class="col-md-12">
@@ -16,8 +41,7 @@
    @csrf
    <!-- FullCalendar CSS -->
    <link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css" rel="stylesheet" />
-   <link rel="stylesheet" type="text/css"
-       href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.print.css" media="print">
+   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.print.css" media="print">
 
    <!-- jQuery -->
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
