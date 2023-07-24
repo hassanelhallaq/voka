e8cf81aa7bd7904fe730ae4911e4521fc946257d
Hassan <hassan.elhallaq93@gmail.com>
2023-07-24 20:38:22 +0300

    client_index

--- resources/views/dashboard/client/index.blade.php
+++ resources/views/dashboard/client/index.blade.php
@@ -3,6 +3,8 @@
             <div id="kt_app_content" class="app-content flex-column-fluid">
                 <!--begin::Content container-->
                 <div id="kt_app_content_container" class="app-container container-xxl">
+                    <a data-bs-toggle="modal" data-bs-target="#clients" class="btn btn-sm fw-bold btn-primary"
+                        data-bs-toggle="modal" data-bs-target="#kt_modal_create_app">{{ __('Create') }}</a>
                     @foreach ($clients as $item)
                         <div class="card mb-5 mb-xxl-8">
                             <div class="card-body pt-9 pb-0">
@@ -69,94 +71,27 @@
                                                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-3"
                                                         data-kt-menu="true">
                                                         <!--begin::Heading-->
-                                                        <div class="menu-item px-3">
-                                                            <div
-                                                                class="menu-content text-muted pb-2 px-3 fs-7 text-uppercase">
-                                                                Payments</div>
-                                                        </div>
+
                                                         <!--end::Heading-->
                                                         <!--begin::Menu item-->
-                                                        <div class="menu-item px-3">
-                                                            <a href="#" class="menu-link px-3">Create Invoice</a>
-                                                        </div>
+
                                                         <!--end::Menu item-->
                                                         <!--begin::Menu item-->
                                                         <div class="menu-item px-3">
-                                                            <a href="#" class="menu-link flex-stack px-3">Create
-                                                                Payment
-                                                                <span class="ms-2" data-bs-toggle="tooltip"
-                                                                    title="Specify a target name for future usage and reference">
-                                                                    <i class="ki-duotone ki-information fs-6">
-                                                                        <span class="path1"></span>
-                                                                        <span class="path2"></span>
-                                                                        <span class="path3"></span>
-                                                                    </i>
-                                                                </span></a>
+
+                                                            <a href="{{ route('clients.show', [$item->id]) }}"
+                                                                class="menu-link px-3">View</a>
+
                                                         </div>
                                                         <!--end::Menu item-->
                                                         <!--begin::Menu item-->
-                                                        <div class="menu-item px-3">
-                                                            <a href="#" class="menu-link px-3">Generate Bill</a>
-                                                        </div>
+
                                                         <!--end::Menu item-->
                                                         <!--begin::Menu item-->
-                                                        <div class="menu-item px-3" data-kt-menu-trigger="hover"
-                                                            data-kt-menu-placement="right-end">
-                                                            <a href="#" class="menu-link px-3">
-                                                                <span class="menu-title">Subscription</span>
-                                                                <span class="menu-arrow"></span>
-                                                            </a>
-                                                            <!--begin::Menu sub-->
-                                                            <div class="menu-sub menu-sub-dropdown w-175px py-4">
-                                                                <!--begin::Menu item-->
-                                                                <div class="menu-item px-3">
-                                                                    <a href="#" class="menu-link px-3">Plans</a>
-                                                                </div>
-                                                                <!--end::Menu item-->
-                                                                <!--begin::Menu item-->
-                                                                <div class="menu-item px-3">
-                                                                    <a href="#" class="menu-link px-3">Billing</a>
-                                                                </div>
-                                                                <!--end::Menu item-->
-                                                                <!--begin::Menu item-->
-                                                                <div class="menu-item px-3">
-                                                                    <a href="#"
-                                                                        class="menu-link px-3">Statements</a>
-                                                                </div>
-                                                                <!--end::Menu item-->
-                                                                <!--begin::Menu separator-->
-                                                                <div class="separator my-2"></div>
-                                                                <!--end::Menu separator-->
-                                                                <!--begin::Menu item-->
-                                                                <div class="menu-item px-3">
-                                                                    <div class="menu-content px-3">
-                                                                        <!--begin::Switch-->
-                                                                        <label
-                                                                            class="form-check form-switch form-check-custom form-check-solid">
-                                                                            <!--begin::Input-->
-                                                                            <input
-                                                                                class="form-check-input w-30px h-20px"
-                                                                                type="checkbox" value="1"
-                                                                                checked="checked"
-                                                                                name="notifications" />
-                                                                            <!--end::Input-->
-                                                                            <!--end::Label-->
-                                                                            <span
-                                                                                class="form-check-label text-muted fs-6">Recuring</span>
-                                                                            <!--end::Label-->
-                                                                        </label>
-                                                                        <!--end::Switch-->
-                                                                    </div>
-                                                                </div>
-                                                                <!--end::Menu item-->
-                                                            </div>
-                                                            <!--end::Menu sub-->
-                                                        </div>
+
                                                         <!--end::Menu item-->
                                                         <!--begin::Menu item-->
-                                                        <div class="menu-item px-3 my-1">
-                                                            <a href="#" class="menu-link px-3">Settings</a>
-                                                        </div>
+
                                                         <!--end::Menu item-->
                                                     </div>
                                                     <!--end::Menu 3-->
@@ -324,6 +259,69 @@
                             </div>
                         </div>
                     @endforeach
+                    <span class="span">
+                        {!! $clients->links() !!}
+                    </span>
+                    <div class="modal fade" id="clients" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered mw-750px">
+                            <div class="modal-content">
+                                <div class="modal-body scroll-y mx-lg-5 my-7">
+                                    <!--begin::Form-->
+                                    <form id="kt_modal_add_role_form" class="form">
+                                        @csrf
+                                        <!--begin::Scroll-->
+                                        <div class="d-flex flex-column scroll-y me-n7 pe-7">
+                                            <div class="row">
+                                                <div class="form-group mb-6">
+                                                    <label>{{ __('Name') }}</label>
+
+                                                    <div class="input-group mb-3">
+                                                        <input type="text" class="form-control meal_price"
+                                                            name="name" required id='name'
+                                                            value="{{ old('name') }}">
+                                                    </div>
+                                                    @if ($errors->has('name'))
+                                                        <p style="color: red">
+                                                            {{ $errors->first('name') }}
+                                                        </p>
+                                                    @endif
+                                                </div>
+                                                <div class="form-group mb-6">
+                                                    <label>{{ __('client phone') }}</label>
+                                                    <div class="input-group mb-3">
+                                                        <input type="number" class="form-control meal_price"
+                                                            name="phone" required id='phone'
+                                                            value="{{ old('phone') }}">
+                                                    </div>
+                                                    @if ($errors->has('phone'))
+                                                        <p style="color: red">
+                                                            {{ $errors->first('phone') }}
+                                                        </p>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--end::Scroll-->
+                                        <!--begin::Actions-->
+                                        <div class="text-center pt-15">
+                                            <button type="reset" class="btn btn-light me-3" data-bs-dismiss="modal"
+                                                data-kt-roles-modal-action="cancel">
+                                                {{ __('Discard') }}</button>
+                                            <button onclick="performStore()" type="z" class="btn btn-primary"
+                                                data-kt-roles-modal-action="submit">
+                                                {{ __('Submit') }}
+                                            </button>
+                                        </div>
+                                        <!--end::Actions-->
+                                    </form>
+                                    <!--end::Form-->
+                                </div>
+                                <!--end::Modal body-->
+                            </div>
+                            <!--end::Modal content-->
+                        </div>
+                        <!--end::Modal dialog-->
+                    </div>
                     <!--end::Navbar-->
                     <!--begin::Row-->
 
