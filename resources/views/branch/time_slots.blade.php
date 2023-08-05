abe4f069c125fff3417bd153466d0ce2bab1a1fb
server <vkoanet@az1-ss106.a2hosting.com>
2023-08-05 05:35:22 -0700

    slots

--- resources/views/branch/time_slots.blade.php
+++ resources/views/branch/time_slots.blade.php
@@ -1,23 +1,26 @@
 <div class="row" style="margin-bottom:50px; margin-top:50px">
     <div class="col-lg-12">
-        @foreach ($timeSlots as $slot)
-            @php
-                $slotClosed = false;
-                foreach ($unavailableSlots as $unavailableSlot) {
-                    if ($slot['start'] === $unavailableSlot['start'] && $slot['end'] === $unavailableSlot['end']) {
-                        $slotClosed = true;
-                        break;
+
+                 @foreach ($timeSlots as $slot)
+                @php
+                    $slotClosed = false;
+                    foreach ($unavailableSlots as $unavailableSlot) {
+                        if ($slot['start'] === $unavailableSlot['start'] && $slot['end'] === $unavailableSlot['end']) {
+                            $slotClosed = true;
+                            break;
+                        }
                     }
-                }
-            @endphp
-            @if ($slotClosed)
-                <button class="btn btn-lg btn-danger btn-clock" disabled>{{ $slot['start'] }} -
-                    {{ $slot['end'] }}</button>
-            @else
-                <button class="btn btn-lg btn-success btn-clock">{{ $slot['start'] }} -
-                    {{ $slot['end'] }}</button>
-            @endif
-        @endforeach
+                @endphp
+                @if ($slotClosed)
+                    <button class="btn btn-lg btn-danger btn-clock" disabled>{{ $slot['start'] }} -
+                        {{ $slot['end'] }}</button>
+                @else
+                    <button class="btn btn-lg btn-success btn-clock">{{ $slot['start'] }} -
+                        {{ $slot['end'] }}</button>
+                @endif
+            @endforeach
+            
+
     </div>
 </div>
 <div class="row">
