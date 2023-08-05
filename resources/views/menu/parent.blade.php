a0d35e28ff28c64922921eebb522fa9755ee1178
server <vkoanet@az1-ss106.a2hosting.com>
2023-08-05 11:39:42 -0700

    slots

--- resources/views/menu/parent.blade.php
+++ resources/views/menu/parent.blade.php
@@ -15,6 +15,7 @@
     <!-- Icon Fonts -->
     <link rel="stylesheet" href="{{ asset('/menu/assets/fonts/flaticon/flaticon.css') }}">
     <link rel="stylesheet" href="{{ asset('/menu/assets/fonts/font-awesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -64,13 +65,16 @@
             <div class="container">
                 <div class="top-header-inner">
                     <ul class="top-header-nav header-cta">
-                        <li> <a href="menu-v1.html">استدعي الويتر</a> </li>
+                        
+                        <li> 
+                       
+                        <a href="#">
+                         <i class="fa-solid fa-bell"></i>
+                        استدعي الويتر
+                        </a>
+                         </li>
                     </ul>
-                </div>
-            </div>
-        </div>
-
-        <div class="container">
+                     <div class="container">
             <nav class="navbar">
                 <!-- Logo -->
                 <a class="navbar-brand"
@@ -102,6 +106,11 @@
 
             </nav>
         </div>
+                </div>
+            </div>
+        </div>
+
+       
 
     </header>
     @yield('content')
