6388e917d8ec0e2c330405a0a206b31d7abe3298
Hassan <hassan.elhallaq93@gmail.com>
2023-08-11 19:25:30 +0300

    new halles

--- resources/views/dashboard/order/reservations.blade.php
+++ resources/views/dashboard/order/reservations.blade.php
@@ -11,7 +11,7 @@
                                 <!--begin::Title-->
                                 <h1
                                     class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">
-                                    {{ __('orders') }}</h1>
+                                    {{ __('reservations') }}</h1>
 
                             </div>
 
