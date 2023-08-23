5abfd034ed1372d7c5fc479cf0ca2869ce496ddc
Hassan <hassan.elhallaq93@gmail.com>
2023-08-23 19:49:09 +0300

    home

--- resources/views/branch/reserv.blade.php
+++ resources/views/branch/reserv.blade.php
@@ -1,99 +1,103 @@
-   @extends('branch.parent')
-   @section('contentFront')
-       <style>
-           .fc-day-number {
-               color: #fff;
-           }
+@extends('branch.parent')
+@section('contentFront')
+    <style>
+        .fc-day-number {
+            color: #fff;
+        }
 
-           .fc-widget-header thead tr {
-               color: #fff;
-               font-size: 20px;
-           }
+        .fc-widget-header thead tr {
+            color: #fff;
+            font-size: 20px;
+        }
 
-           tbody td {
-               border-radius: 0;
-           }
+        tbody td {
+            border-radius: 0;
+        }
 
-           .fc-unthemed td.fc-today .fc-day-number {
-               color: #fff;
-           }
+        .fc-unthemed td.fc-today .fc-day-number {
+            color: #fff;
+        }
 
-           .fc-state-active,
-           .fc-state-disabled,
-           .fc-state-down,
-           .fc-state-top,
-           .fc-state-hover {
-               color: #fff !important;
-               background-color: #e5772a !important;
-           }
+        .fc-state-active,
+        .fc-state-disabled,
+        .fc-state-down,
+        .fc-state-top,
+        .fc-state-hover {
+            color: #fff !important;
+            background-color: #e5772a !important;
+        }
 
-           .fc-state-hover {
-               background-position: 0 2.1em !important;
-           }
+        .fc-state-hover {
+            background-position: 0 2.1em !important;
+        }
 
-           tbody td:hover {
-               border: 1px solid #ddd;
-           }
+        tbody td:hover {
+            border: 1px solid #ddd;
+        }
 
-           .fc-unthemed td.fc-today {
-               background-color: #e5772a !important;
-           }
+        .fc-unthemed td.fc-today {
+            background-color: #e5772a !important;
+        }
 
-           tbody tr {
-               color: #fff;
-           }
+        tbody tr {
+            color: #fff;
+        }
 
-           .fc-event,
-           .fc-event-dot {
-               background-color: #d59161 !important;
-           }
-       </style>
-       <div id="mainPage" class="reves-main">
-           <div class="container mt-5">
-               <div id="calendar">
-               </div>
-           </div>
-       </div>
-       <meta name="csrf-token" content="{{ csrf_token() }}" />
-       @csrf
-   @endsection
+        .fc-event,
+        .fc-event-dot {
+            background-color: #d59161 !important;
+        }
+    </style>
+    <div id="mainPage" class="reves-main">
+        <div class="container mt-5">
+            <div id="calendar">
+            </div>
+        </div>
+    </div>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    @csrf
+@endsection
+@section('js')
+    <!-- jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- Moment.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/1.6.4/fullcalendar.print.css" rel="stylesheet" type="text/css"
+        media="print" />
+    <script>
+        $(document).ready(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            var calendar = $('#calendar').fullCalendar({
+                editable: true,
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay',
+                },
+                events: "{{ route('ajaxCalender') }}",
+                selectable: true,
+                selectHelper: true,
+                firstDay: 0,
+                eventClick: function(event) {
+                    $.get('/branch/path/to/branch.reservSide', {
+                        id: event.id,
+                    }).done(function(data) {
+                        $('#reservSideContainer').html(data); // Show the new content
+                        $('#reservSideContainer #reservSideContainer').css("width", "100%");
+                    });
+                },
 
-   <!-- jQuery -->
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-   <!-- Moment.js -->
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
-   <script>
-       $(document).ready(function() {
-           $.ajaxSetup({
-               headers: {
-                   'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-               }
-           });
-           var calendar = $('#calendar').fullCalendar({
-               editable: true,
-               header: {
-                   left: 'prev,next today',
-                   center: 'title',
-                   right: 'month,agendaWeek,agendaDay',
-               },
-               events: "{{ route('ajaxCalender') }}",
-               selectable: true,
-               selectHelper: true,
-               firstDay: 0,
-               eventClick: function(event) {
-                   $.get('/branch/path/to/branch.reservSide', {
-                       id: event.id,
-                   }).done(function(data) {
-                       $('#reservSideContainer').html(data); // Show the new content
-                       $('#reservSideContainer #reservSideContainer').css("width", "100%");
-                   });
-               },
+            });
 
-           });
+            $('#mainPage').removeClass('col-md-11').addClass('col-md-8');
 
-           $('#mainPage').removeClass('col-md-11').addClass('col-md-8');
-
-       });
-   </script>
+        });
+    </script>
+@endsection
