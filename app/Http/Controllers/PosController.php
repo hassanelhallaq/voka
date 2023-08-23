d56f6c07d477bc2bb401a4cdb38665182b641f98
Hassan <hassan.elhallaq93@gmail.com>
2023-08-23 17:49:52 +0300

    home

--- app/Http/Controllers/PosController.php
+++ app/Http/Controllers/PosController.php
@@ -337,41 +337,49 @@ class PosController extends Controller
         $package = Package::find($request->packageId);
         $minutesPerPackage = $package->time;
 
-        // Calculate the available time slots
-        $availableSlots = [];
-        $unavailableSlots = [];
+        // Generate time slots based on the package minutes
+        $startTime = Carbon::createFromTime(0, 0, 0);
+        $endTime = Carbon::createFromTime(23, 59, 59);
+        $timeSlots = [];
 
-        $prevEndTime = $now; // Start with the current time
+        $currentTime = clone $startTime;
+        while ($currentTime->lte($endTime)) {
+            $endTimeSlot = clone $currentTime;
+            $endTimeSlot->addMinutes($minutesPerPackage);
+
+            // Check if the time slot is in the future
+            if ($endTimeSlot->isFuture()) {
+                $timeSlots[] = [
+                    'start' => $currentTime->format('g:i A'),
+                    'end' => $endTimeSlot->format('g:i A'),
+                ];
+            }
+
+            $currentTime->addMinutes($minutesPerPackage);
+        }
+
+        // Calculate the unavailable time slots
+        $unavailableSlots = [];
         foreach ($reservations as $reservation) {
             $start = Carbon::parse($reservation->date);
             $end = Carbon::parse($reservation->end);
 
-            if ($prevEndTime->lt($start)) {
-                $availableSlots[] = [
-                    'start' => $prevEndTime->format('g:i A'),
-                    'end' => $start->format('g:i A'),
-                ];
-            }
             $unavailableSlots[] = [
                 'start' => $start->format('g:i A'),
                 'end' => $end->format('g:i A'),
             ];
-
-            $prevEndTime = $end;
         }
 
-        $endTime = Carbon::createFromTime(23, 59, 59);
-        if ($prevEndTime->lt($endTime)) {
-            $availableSlots[] = [
-                'start' => $prevEndTime->format('g:i A'),
-                'end' => $endTime->format('g:i A'),
-            ];
-        }
+        // Calculate the available time slots by removing reserved slots from all slots
+        $availableSlots = array_filter($timeSlots, function ($slot) use ($unavailableSlots) {
+            foreach ($unavailableSlots as $unavailableSlot) {
+                if ($slot['start'] === $unavailableSlot['start'] && $slot['end'] === $unavailableSlot['end']) {
+                    return false; // Slot is reserved, so it's not available
+                }
+            }
+            return true; // Slot is available
+        });
 
-        // Now you can use the $availableSlots and $unavailableSlots arrays as needed
-
-        // Generate the time slots for HTML integration
-        $timeSlots = $availableSlots;
         return  $render = view('branch.time_slots', compact('availableSlots', 'unavailableSlots', 'timeSlots'));
         // return response()->view('branch.calender');
     }
